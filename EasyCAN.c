08c3782042e9403a30258026ba5cdc842e07a2a2
Tonio331 <102752632+Tonio331@users.noreply.github.com>
2024-03-16 09:48:05 +0100

    Add files via upload

--- EasyCAN.c
+++ EasyCAN.c
@@ -1,10 +1,31 @@
 #include "EasyCAN.h"
 #include "can.h"
+#include "pwm.h"
+#include "interrupt.h"
+#include "string.h"
 
 
 #define WHEELBOARD_ADDRESS 0x020
+#define TUNING_ADDRESS 0x040
 
 WHEELBOARD_BUTTONS buttons;
+TUNINGPARAM tuning;
+uint32_t lastWheelBoardTime;
+uint8_t canTx[8];
+CAN_FilterTypeDef canFil;
+
+void ConfigCAN(){
+	canFil.FilterBank = 10;
+	canFil.FilterMode = CAN_FILTERMODE_IDMASK;
+	canFil.FilterFIFOAssignment = CAN_RX_FIFO0;
+	canFil.FilterIdHigh = 0;
+	canFil.FilterIdLow = 0;
+	canFil.FilterMaskIdHigh = 0;
+	canFil.FilterMaskIdLow = 0;
+	canFil.FilterScale = CAN_FILTERSCALE_32BIT;
+	canFil.FilterActivation = ENABLE;
+	canFil.SlaveStartFilterBank = 0;
+}
 
 void sendCAN(uint16_t ADDRESS,uint8_t Tx[8],uint8_t dataLen){
 
@@ -24,6 +45,7 @@ void sendCAN(uint16_t ADDRESS,uint8_t Tx[8],uint8_t dataLen){
 		}
 	}
 	if(flag){
+		flag=1;
 		//generic flag to alert about overflowed Mailbox
 	}
 
@@ -37,13 +59,36 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
 
 	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rxHeader, canRx);
 
-	if(rxHeader.StdId==WHEELBOARD_ADDRESS){
-			buttons.strategy=canRx[0];
-			buttons.motorON=canRx[1];
-			buttons.purge=canRx[2];
-			buttons.powermode=canRx[3];
-			buttons.shortFC=canRx[4];
+	switch(rxHeader.StdId){
+	case WHEELBOARD_ADDRESS:
+		buttons.strategy=canRx[0];
+		buttons.motorON=canRx[1];
+		buttons.purge=canRx[2];
+		buttons.powermode=canRx[3];
+		buttons.shortFC=canRx[4];
+		lastWheelBoardTime=HAL_GetTick();
+		break;
+	case TUNING_ADDRESS:
+		tuning.p=canRx[0];
+		tuning.i=canRx[1];
+		tuning.d=canRx[2];
+		break;
 	}
+}
 
+void Transmit(){
+	memcpy(canTx,&motorAmps,4);
+    sendCAN(0x030,canTx,4);
+    duty=pwmConfMotor.duty;
+    memcpy(canTx,&duty,4);
+    sendCAN(0x031,canTx,4);
+    canSendFlag=0;
+}
 
+void resetWheelBoard(){
+	  buttons.motorON=0;
+	  buttons.strategy=0;
+	  buttons.purge=0;
+	  buttons.powermode=0;
+	  buttons.shortFC=0;
 }
--- EasyCAN.h
+++ EasyCAN.h
@@ -19,7 +19,25 @@ typedef struct {
 	uint8_t shortFC;
 }WHEELBOARD_BUTTONS;
 
+typedef struct {
+	uint8_t p;
+	uint8_t i;
+	uint8_t d;
+}TUNINGPARAM;
+
+extern WHEELBOARD_BUTTONS buttons;
+extern TUNINGPARAM tuning;
+extern uint8_t canTx[8];
+extern uint32_t lastWheelBoardTime;
+extern CAN_FilterTypeDef canFil;
+
+void ConfigCAN();
+
+void Transmit();
+
 void sendCAN( uint16_t ADDRESS , uint8_t Tx[8] , uint8_t dataLen );
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan);
+
+void resetWheelBoard();
 #endif /* INC_EASYCAN_H_ */
